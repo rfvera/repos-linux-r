f8d0c7b6332777c24172c59f3b2a5a028a243b5c
rfvera <rfvera@gmail.com>
2023-08-15 23:27:13 -0400

    dAFD

---
+++ prueba desde linux.R
@@ -0,0 +1,22 @@
+library (usethis)
+create_github_token()
+library (gitcreds)
+gitcreds::gitcreds_set() ###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
+
+datos<-rnorm(1000)
+
+use_github()
+hist(datos)
+summary<-summary(datos)
+prueba<- datos
+
+
+##*** Por favor cuéntame quién eres.
+
+###Ejecuta
+
+git config --global user.email "rfvera@gmail.com"
+git config --global user.name "rfvera"
+
+###para otra prueba xxgiujrvckhglkhgv
+njjnchgjdhjsh
